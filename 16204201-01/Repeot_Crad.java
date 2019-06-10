5b71f195d9fe23e29e485fe71e51c445766e8ed0
s16204201 <adong1996@yeah.net>
2019-06-10 17:51:20 +0800

    16204201-01

---
+++ 16204201-01/Repeot_Crad.java
@@ -0,0 +1,26 @@
+package Web.Subject.domain;
+
+public class Repeot_Crad {
+	private int id;
+	private String course;
+	private double grade;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public double getGrade() {
+		return grade;
+	}
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+	
+}
